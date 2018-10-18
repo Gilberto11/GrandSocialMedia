c6208917612ba7e6cee5bd491cad48011e6219b8
Graham Farrell <grahamfarrell1984@gmail.com>
2018-10-18 20:08:38 +0100

    Removed some code. Added link to config.php file.

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-
-$con = mysqli_connect("localhost", "root", "", "social");  //Connection variable
-
-if(mysqli_connect_errno()) {
-
-	echo "Failed to connect: " . mysqli_connect_errno();
-
-}
-
-$query = mysqli_query($con, "INSERT INTO test VALUES ('1', 'Snakes')");
+require 'config/config.php';
 
 ?>
 
