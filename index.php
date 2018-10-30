73cafc2fee0908c9a0d523b173c2f83f52a718e1
Graham Farrell <grahamfarrell1984@gmail.com>
2018-10-30 20:26:47 +0000

    Added code to add num_posts and num_likes under profile picture.

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ include("includes/header.php");
 	<div class= "user_details column">
 		<a href="#"><img src= "<?php echo $user['profile_pic']?>"></a>
 
+		<a href="#">
 		<?php 
 
 			echo $user['first_name'] . " " . $user['last_name'];
 
-		 ?>
+		?>
+		</a>
+		<?php echo "Posts: " . $user['num_posts']. "<br>";
+
+			echo "Likes: " . $user['num_likes'];
+
+		?>
 
 	</div>
 
