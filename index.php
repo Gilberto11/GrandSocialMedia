f57532dc0de0070e8c286c67192bff2553e59b5b
Graham Farrell <grahamfarrell1984@gmail.com>
2018-10-30 20:20:34 +0000

    Added code to add user name beside profile picture.

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ include("includes/header.php");
 	<div class= "user_details column">
 		<a href="#"><img src= "<?php echo $user['profile_pic']?>"></a>
 
+		<?php 
+
+			echo $user['first_name'] . " " . $user['last_name'];
+
+		 ?>
+
 	</div>
 
 
