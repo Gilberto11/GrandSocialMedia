fa863fb0354f3f307b058d53e44b197ff07cbc7a
gilberto <surfcorinthians@gmail.com>
2018-11-28 11:28:06 +0000

    fixing minor but of extra b> on the comments

--- comment_frame.php
+++ comment_frame.php
@@ -142,7 +142,7 @@ if ($count != 0) {
 
         <div class="comment_section">
 		<a href="<?php echo $posted_by?>" target = "_parent"><img src="<?php echo $user_obj -> getProfilePic();?>" title="<?php echo $posted_by; ?>" style="float:left;" height="30"></a>
-		<a href="<?php echo $posted_by?>" target="_parent"> <B><?php echo $user_obj->getFirstAndLastName(); ?> </B>b></a>
+		<a href="<?php echo $posted_by?>" target="_parent"> <B><?php echo $user_obj->getFirstAndLastName(); ?> </B></a>
 		&nbsp;&nbsp;&nbsp;&nbsp; <?php echo $time_message . "<br>" . $comment_body; ?>
 		<hr>
 	</div>
