0b29e84274ef6c26476897de823bfbedfc9ab8d1
gilberto <surfcorinthians@gmail.com>
2018-12-02 09:47:18 +0000

    adding the classes and linking it with the user and post files

--- like.php
+++ like.php
@@ -5,5 +5,21 @@
 </head>
 <body>
 	
+	<?php
+    require 'config/config.php';
+    include("includes/classes/User.php");
+    include("includes/classes/Post.php");
+    
+
+if (isset($_SESSION['username'])){
+    $userLoggedIn = $_SESSION['username']; // check if the user is logged in, if not the page will force the user to registration form
+  $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$userLoggedIn'");
+  $user = mysqli_fetch_array($user_details_query);
+}
+else{
+    header("location: register.php");
+}
+
+?>
 </body>
 </html>
