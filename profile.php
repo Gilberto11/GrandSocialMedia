75b24f141611606a840d0abb5fd81f458bb8c895
BossRymes <craigjohnshiels@hotmail.com>
2018-12-08 13:56:28 +0000

    profile name

--- profile.php
+++ profile.php
@@ -1,9 +1,17 @@
 <?php
 include("includes/header.php");
+
+if(isset($_GET['profile_username'])) {
+	$username = $_GET['profile_username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username= '$username'");
+	$user_array = mysqli_fetch_array($user_details_query);
+}
+
+
 ?>
 	
 	<div  class="main_column column">
-		This is a profile page
+	 <?php echo $username; ?>
 		
 			
 
