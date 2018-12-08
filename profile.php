0e03716f87a749f90232d2241d8d994d04a27221
dominic dillon <domodillon82@gmail.com>
2018-12-08 16:32:33 +0000

    friend request button

--- profile.php
+++ profile.php
@@ -48,10 +48,24 @@ if(isset($_GET['profile_username'])) {
 				echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
 			}
 
-		}
+			else if($logged_in_user_obj->didReceiveRequest($username)){
+				echo '<input type="submit" name="respond_request" class="warning" value="Respond to Request"><br>';
+			}
+
+			else if($logged_in_user_obj->didSendRequest($username)){
+				echo '<input type="submit" name="" class="default" value="Request Sent"><br>';
+			}
+
+			else
+				echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
+			}
+
+		
 
 
 		?>
+
+	</form>
 
 </div>
 
