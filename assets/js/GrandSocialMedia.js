c2fe2bf00e8769785b6ab7420c1a27dda5326ff4
gilberto <surfcorinthians@gmail.com>
2018-12-09 23:14:28 +0000

    fix small bug on posting from profile

--- assets/js/GrandSocialMedia.js
+++ assets/js/GrandSocialMedia.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
 		$.ajax({
 			type: "POST",
 			url: "includes/handlers/ajax_submit_profile_post.php",
-			data: $('form.profile_post').serialized(),
+			data: $('form.profile_post').serialize(),
 			success: function(msg) {
 				$("#post_form").modal('hide');
 				location.reload();
 			},
 			error: function() {
-				alert('failure');
+				alert('Failure');
 			}
 		});
 
